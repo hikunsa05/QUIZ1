f92836ad2a676336e37cc0f8c76b9fdf89e8388f
hikunsa05 <113221885+hikunsa05@users.noreply.github.com>
2023-09-29 20:59:23 +0700

    Add files via upload

---
+++ QUIZ1_220441100126_FIRA FASICHAH FAUZI/src/main/java/com/mycompany/quiz1/Quiz1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quiz1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Quiz1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
